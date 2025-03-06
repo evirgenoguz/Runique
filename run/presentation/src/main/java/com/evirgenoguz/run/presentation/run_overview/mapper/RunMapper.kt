6b2db87efc3e485e441a74d802eade3d46313fc0
evirgenoguz <evirgenoguz@gmail.com>
2025-03-06 10:22:05 +0300

    Tracking Run
    - add: run domain model and ui model
    - add: mapper for domain model to ui model

---
+++ run/presentation/src/main/java/com/evirgenoguz/run/presentation/run_overview/mapper/RunMapper.kt
@@ -0,0 +1,33 @@
+package com.evirgenoguz.run.presentation.run_overview.mapper
+
+import com.evirgenoguz.core.domain.run.Run
+import com.evirgenoguz.core.presentation.ui.formatted
+import com.evirgenoguz.core.presentation.ui.toFormattedKm
+import com.evirgenoguz.core.presentation.ui.toFormattedKmh
+import com.evirgenoguz.core.presentation.ui.toFormattedMeters
+import com.evirgenoguz.core.presentation.ui.toFormattedPace
+import com.evirgenoguz.run.presentation.run_overview.RunUi
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+fun Run.toRunUi(): RunUi {
+    val dateTimeInLocalTime = dateTimeUtc.withZoneSameInstant(ZoneId.systemDefault())
+
+    val formattedDateTime = DateTimeFormatter
+        .ofPattern("MMM dd, yyyy - hh:mma")
+        .format(dateTimeInLocalTime)
+
+    val distanceKm = distanceMeters / 1000.0
+
+    return RunUi(
+        id = id!!,
+        duration = duration.formatted(),
+        dateTime = formattedDateTime,
+        distance = distanceKm.toFormattedKm(),
+        avgSpeed = avgSpeedKmH.toFormattedKmh(),
+        maxSpeed = maxSpeedKmH.toFormattedKmh(),
+        pace = duration.toFormattedPace(distanceKm),
+        totalElevation = totalElevationMeters.toFormattedMeters(),
+        mapPictureUrl = mapPictureUrl
+    )
+}
