8edbf1549ae6e4e1045d7ceb5f888c6ce07253d4
evirgenoguz <evirgenoguz@gmail.com>
2025-03-22 01:25:51 +0300

    Tracking Run Synchronization and Deletion
    - add: `RunPendingSyncEntity` and `DeletedRunSyncEntity` to track pending create and delete actions on runs
    - add: `RunPendingSyncDao` to manage pending sync entities for runs
    - add: `syncPendingRuns` function on `RunRepository` to synchronize pending runs
    - add: logic for handling pending run deletions and creations in `deleteRun` on `OfflineFirstRunRepository`
    - add: add run pending sync dao to `DatabaseModule`
    - add: `syncPendingRuns` call to `RunOverviewViewModel` to start synchronization.
    - fix: use correct `serialization` api

---
+++ core/database/src/main/java/com/evirgenoguz/core/database/dao/RunPendingSyncDao.kt
@@ -0,0 +1,37 @@
+package com.evirgenoguz.core.database.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import com.evirgenoguz.core.database.entity.DeletedRunSyncEntity
+import com.evirgenoguz.core.database.entity.RunPendingSyncEntity
+
+@Dao
+interface RunPendingSyncDao {
+
+    // CREATED RUNS
+    @Query("Select * From runpendingsyncentity Where userId = :userId")
+    suspend fun getAllRunPendingSyncEntities(userId: String): List<RunPendingSyncEntity>
+
+    @Query("Select * From runpendingsyncentity Where runId = :runId")
+    suspend fun getRunPendingSyncEntities(runId: String): RunPendingSyncEntity?
+
+    @Upsert
+    suspend fun upsertRunPendingSyncEntity(entity: RunPendingSyncEntity)
+
+    @Query("Delete From runpendingsyncentity Where runId = :runId")
+    suspend fun deleteRunPendingSyncEntity(runId: String)
+
+
+    // DELETED RUNS
+
+    @Query("SELECT * FROM deletedrunsyncentity WHERE userId=:userId")
+    suspend fun getAllDeletedRunSyncEntities(userId: String): List<DeletedRunSyncEntity>
+
+    @Upsert
+    suspend fun upsertDeletedRunSyncEntity(entity: DeletedRunSyncEntity)
+
+    @Query("DELETE FROM deletedrunsyncentity WHERE runId=:runId")
+    suspend fun deleteDeletedRunSyncEntity(runId: String)
+
+}
