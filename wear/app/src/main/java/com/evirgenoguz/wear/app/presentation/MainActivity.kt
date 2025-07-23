f443e98eabee1b281d2b9f5dbe9decdc80e98ba1
evirgenoguz <evirgenoguz@gmail.com>
2025-07-23 18:41:47 +0300

    feat: Add initial Wear OS module with theme, UI components, and test cases

--- wear/app/src/main/java/com/evirgenoguz/wear/app/presentation/MainActivity.kt
+++ wear/app/src/main/java/com/evirgenoguz/wear/app/presentation/MainActivity.kt
@@ -13,7 +13,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.wear.compose.material3.Button
 import androidx.wear.compose.material3.Text
+import com.evirgenoguz.core.presentation.designsystem_wear.RuniqueTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,11 +24,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(text = "Hello from Wear!")
+            RuniqueTheme {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Button(onClick = { }) {
+                        Text(text = "Open Runique")
+                    }
+                }
             }
         }
     }
